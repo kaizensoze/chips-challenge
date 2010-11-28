0c5f43ab3a90954dc194ee75877a9eda4eae5ecb
duckspeaker <gallo.j@gmail.com>
2010-11-28 17:24:34 -0500

    added name to json map files

--- js/chip_new.js
+++ js/chip_new.js
@@ -1,9 +1,12 @@
 
 var MapEditor = function() {
+    // TODO: change this to array of Map objects
     var maps = {};
     return {
         getMaps : function() {
             $.getJSON('php/mapeditor.php?action=load_maps', function(data) {
+                // TODO: iterate over json, stuff each into Map object, add to array of Map objects
+                //       don't use associative array; store both name, level in json
                 maps = JSON.parse(JSON.stringify(data)); // stringify is required ::shrug::
             });
         }
