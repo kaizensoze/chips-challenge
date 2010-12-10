91d68713fd266fa4d99ee099e4d15933d764dce7
duckspeaker <gallo.j@gmail.com>
2010-12-09 23:14:46 -0500

    restructure

--- js/mapeditor.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function() {
-    var map_editor = new MapEditor();
-    var map_editor_gui = new MapEditorGUI(map_editor);
-    map_editor_gui.loadMaps();
-    map_editor_gui.loadTiles();
-});
-
