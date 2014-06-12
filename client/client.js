08fd8cf8212089dda07b40a99f637064c8bcb404
Joe Gallo <gallo.j@gmail.com>
2014-06-12 02:27:24 -0400

    Pick up items. Unlock locks.

--- client/client.js
+++ client/client.js
@@ -120,7 +120,7 @@ function startLevel() {
   centerChip();
 
   // clear inventory
-  Session.set('inventory', {});
+  Session.set('inventory', []);
 
   // initialize session data for time remaining, chipsLeft
   Session.set('timeRemaining', Session.get('currentLevel').timeLimit);
@@ -130,14 +130,14 @@ function startLevel() {
   startLevelTimer();
 
   // test picking up items
-  // pickUpItem(tiles['redKey']);
-  // pickUpItem(tiles['blueKey']);
-  // pickUpItem(tiles['yellowKey']);
-  // pickUpItem(tiles['greenKey']);
-  // pickUpItem(tiles['iceSkates']);
-  // pickUpItem(tiles['suctionBoots']);
-  // pickUpItem(tiles['fireBoots']);
-  // pickUpItem(tiles['flippers']);
+  // pickupItem(null, tiles['redKey']);
+  // pickupItem(null, tiles['blueKey']);
+  // pickupItem(null, tiles['yellowKey']);
+  // pickupItem(null, tiles['greenKey']);
+  // pickupItem(null, tiles['iceSkates']);
+  // pickupItem(null, tiles['suctionBoots']);
+  // pickupItem(null, tiles['fireBoots']);
+  // pickupItem(null, tiles['flippers']);
 
   // removeItem(tiles['blueKey']);
   // removeItem(tiles['iceSkates']);
@@ -176,10 +176,22 @@ function updateTimeRemaining() {
  * @param  {[type]} item [description]
  * @return {[type]}      [description]
  */
-function pickUpItem(item) {
-  var inv = Session.get('inventory');
-  inv[item.name] = item;
-  Session.set('inventory', inv);
+function pickupItem(tile, item) {
+  // remove item from tile
+  var itemIndex = tile.tileStack.indexOf(item);
+  tile.tileStack.splice(itemIndex, 1);
+
+  // if chip item, decrement 'chips left'
+  if (_.isEqual(item, tiles['chipItem'])) {
+    var chipsLeft = Session.get('chipsLeft');
+    chipsLeft--;
+    Session.set('chipsLeft', chipsLeft);
+  } else {
+    // add non-chip item to inventory
+    var inv = Session.get('inventory');
+    inv.push(item);
+    Session.set('inventory', inv);
+  }
 }
 
 /**
@@ -189,8 +201,41 @@ function pickUpItem(item) {
  */
 function removeItem(item) {
   var inv = Session.get('inventory');
-  delete inv[item.name];
+  var itemIndex = inv.indexOf(item);
+  inv.splice(itemIndex, 1);
   Session.set('inventory', inv);
+}
+
+function unlockLock(tile, lock) {
+  var unlockSucceeded = false;
+
+  var inv = Session.get('inventory');
+
+  // check for required key. if we have it, unlock the lock.
+  var requiredKey = _.find(inv, function(item) {
+    return item.name === lock.name.replace('Lock', '') + 'Key';
+  });
+
+  var haveKey = _.find(inv, function(item) {
+    return _.isEqual(item, requiredKey);
+  });
+
+  if (haveKey) {
+    // remove lock from tile
+    var lockIndex = tile.tileStack.indexOf(lock);
+    tile.tileStack.splice(lockIndex, 1);
+
+    // remove key from inventory (exception for green key)
+    if (requiredKey.name !== 'greenKey' && /*!lastGreenLock*/) {  // TODO
+      var keyIndex = inv.indexOf(requiredKey);
+      inv.splice(keyIndex, 1);
+      Session.set('inventory', inv);
+    }
+    
+    unlockSucceeded = true;
+  }
+
+  return unlockSucceeded;
 }
 
 /**
@@ -220,31 +265,49 @@ function move(keyCode) {
       newPosition.x++;
       break;
   }
-
-  // TODO: check if move allowed
   
   var currentLevel = Session.get('currentLevel');
 
-  // remove old chip position
+  // old tile
   var currentTile = _.find(currentLevel.levelData, function(mapTile) {
     return mapTile.x == currentPosition.x && mapTile.y == currentPosition.y;
   });
 
-  var currentChipTile = _.find(currentTile.tileStack, function(tile) {
-    return typeof tile.types !== 'undefined' && tile.types.indexOf('chip') !== -1;
-  });
-
-  var currentChipTileIndex = currentTile.tileStack.indexOf(currentChipTile);
-  currentTile.tileStack.splice(currentChipTileIndex, 1);
-  
-  // add new chip position
+  // new tile
   var newTile = _.find(currentLevel.levelData, function(mapTile) {
     return mapTile.x == newPosition.x && mapTile.y == newPosition.y;
   });
 
+  // interact with new tile
+  if (tileContainsType(newTile, 'wall')) {
+    if (tileContainsType(newTile, 'lock')) {
+      var lock = getTileOfType(newTile, 'lock');
+      var unlocked = unlockLock(newTile, lock);
+      if (!unlocked) {
+        return;
+      }
+    } else {
+      // newTile = clone(currentTile);
+      // newPosition = clone(currentPosition);
+      return;
+    }
+  } else if (tileContainsType(newTile, 'item')) {
+    var item = getTileOfType(newTile, 'item');
+    pickupItem(newTile, item);
+  }
+
+  // remove old chip from map
+  var chipTile = _.find(currentTile.tileStack, function(tile) {
+    return 'types' in tile && _.contains(tile.types, 'chip');
+  });
+
+  var chipTileIndex = currentTile.tileStack.indexOf(chipTile);
+  currentTile.tileStack.splice(chipTileIndex, 1);
+
+  // add new chip to map
   newTile.tileStack.push(tiles['chip' + DIRECTION_LABELS[direction]]);
 
-  // update map data and chip position
+  // update level data: map, chip position
   Session.set('currentLevel', currentLevel);
 
   Session.set('currentPosition', newPosition);
@@ -270,6 +333,43 @@ function centerChip() {
   var map = document.getElementById('level-map');
   map.style.left = left + "px";
   map.style.top = top + "px";
+}
+
+/**
+ * Check if a tile has a given type.
+ */
+function tileContainsType(tile, type) {
+  var tiles = _.filter(tile.tileStack, function(tile) {
+    return 'types' in tile && _.contains(tile.types, type);
+  });
+  return tiles.length > 0;
+}
+
+function getTileOfType(tile, type) {
+  var tile = _.find(tile.tileStack, function(tile) {
+    return 'types' in tile && _.contains(tile.types, type);
+  });
+  return tile;
+}
+
+/**
+ * Pad number.
+ * @param  {[type]} n     [description]
+ * @param  {[type]} width [description]
+ * @param  {[type]} z     [description]
+ * @return {[type]}       [description]
+ */
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+/**
+ * Clone. Does a deep clone.
+ */
+function clone(obj) {
+  return JSON.parse(JSON.stringify(obj));
 }
 
 /**
@@ -372,23 +472,3 @@ Template.tilecheck.tiles = function() {
     return tiles[key];
   });
 };
-
-/**
- * Pad number.
- * @param  {[type]} n     [description]
- * @param  {[type]} width [description]
- * @param  {[type]} z     [description]
- * @return {[type]}       [description]
- */
-function pad(n, width, z) {
-  z = z || '0';
-  n = n + '';
-  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
-
-/**
- * Clone. Does a deep clone.
- */
-function clone(obj) {
-  return JSON.parse(JSON.stringify(obj));
-}
