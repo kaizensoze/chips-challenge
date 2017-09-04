bde067bc5b46e2538fb60b07eb35c2de13ef399d
Joe Gallo <joe@brassafrax.com>
2017-09-04 11:59:04 -0400

    x

--- map-editor/app.js
+++ map-editor/app.js
@@ -1,17 +1,25 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const View = requirie('koa-views');
+const send = require('koa-send');
+const serve = require('koa-static');
 
 const app = new Koa();
 const router = new Router();
-const view = new View();
 
-router.get('/', (ctx, next) => {
-  ctx.body = 'Hello Koa';
+app.use(serve('.'));
+app.use(serve('../data/levels'));
+
+router.get('/blah', function (ctx, next) {
+  ctx.body = 'blah';
 });
 
 app
   .use(router.routes())
   .use(router.allowedMethods());
+
+// default route
+app.use(async (ctx) => {
+  await send(ctx, __dirname + '/index.html');
+});
 
 app.listen(3000);
